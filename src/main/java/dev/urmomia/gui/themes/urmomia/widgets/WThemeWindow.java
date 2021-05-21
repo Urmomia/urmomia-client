7eb1af2b2621f0eccb17eecca89eff4524018b34
honsda <spe6games@gmail.com>
2021-05-21 20:23:08 +0700

    please help my neighbour is partying

--- src/main/java/dev/urmomia/gui/themes/urmomia/widgets/WThemeWindow.java
+++ src/main/java/dev/urmomia/gui/themes/urmomia/widgets/WThemeWindow.java
@@ -56,8 +56,7 @@ public class WThemeWindow extends WWindow implements ThemeWidget {
         @Override
         protected void onRender(GuiRenderer renderer, double mouseX, double mouseY, double delta) {
             if (!(forcehh) && !(theme().gradient.get()) || hh && theme().gradient.get()) renderer.quad(this, theme().accentColor.get());
-            if (!(hh) && theme().gradient.get()) renderer.horizontalGradientQuad(this, theme().accentColor.get(), theme().accentColor2.get());
-            if (forcehh && !(theme().gradient.get())) renderer.horizontalGradientQuad(this, theme().accentColor.get(), theme().accentColor2.get());
+            if (forcehh && !(theme().gradient.get()) || !(hh) && theme().gradient.get()) renderer.horizontalGradientQuad(this, theme().accentColor.get(), theme().accentColor2.get());
         }
     }
 }
